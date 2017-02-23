53016c5d4d2247db283edd222b75d9c45d2845d9
Václav Klusák <vaclav.klusak@klokantech.com>
2017-02-23 16:26:58 +0100

    Fix setup.py.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Flask-Gitkit',
     version='1.0',
     description='Google Identity Toolkit integration for Flask',
-    packages=['flask_gitkit'],
+    py_modules=['flask_gitkit'],
     install_requires=[
         'Flask>=0.11',
         'identity-toolkit-python-client>=0.1',
